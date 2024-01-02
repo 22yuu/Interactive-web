782ac40de6126a07290368a3042583e1d42fbe28
yuu <22yuu@naver.com>
2024-01-02 22:14:22 +0900

    feat: 5. 미니게임

--- lectures/part2/5. 미니게임/js/App.js
+++ lectures/part2/5. 미니게임/js/App.js
@@ -1,5 +1,10 @@
 //@ts-nocheck
 import Background from "./Background.js";
+import Coin from "./Coin.js";
+import GameHandler from "./GameHandler.js";
+import Player from "./Player.js";
+import Score from "./Score.js";
+import Wall from "./Wall.js";
 
 export default class App {
   static canvas = document.querySelector("canvas");
@@ -15,18 +20,21 @@ export default class App {
       new Background({ img: document.querySelector("#bg2-img"), speed: -2 }),
       new Background({ img: document.querySelector("#bg1-img"), speed: -4 }),
     ];
-    window.addEventListener("resize", this.resize.bind(this));
+    this.gameHandler = new GameHandler(this);
+    this.reset();
   }
 
-  resize() {
+  reset() {
+    this.walls = [new Wall({ type: "SMALL" })];
+    this.coins = [];
+    this.player = new Player();
+    this.score = new Score();
+  }
+
+  init() {
     App.canvas.width = App.width * App.dpr;
     App.canvas.height = App.height * App.dpr;
     App.ctx.scale(App.dpr, App.dpr);
-
-    const width =
-      innerWidth > innerHeight ? innerHeight * 0.9 : innerWidth * 0.9;
-    App.canvas.style.width = width + "px";
-    App.canvas.style.height = width * (3 / 4) + "px";
   }
 
   render() {
@@ -36,15 +44,90 @@ export default class App {
     const frame = () => {
       requestAnimationFrame(frame);
       now = Date.now();
-      delta = now - delta;
+      delta = now - then;
 
       if (delta < App.interval) return;
+
+      // 배경 관련
+      this.backgrounds.forEach((background) => {
+        background.draw();
+      });
+
+      if (this.gameHandler.status !== "PLAYING") return;
+
       App.ctx.clearRect(0, 0, App.width, App.height);
 
+      // 배경 관련
       this.backgrounds.forEach((background) => {
         background.update();
         background.draw();
       });
+
+      // 벽 관련
+      for (let i = this.walls.length - 1; i >= 0; i--) {
+        this.walls[i].update();
+        this.walls[i].draw();
+
+        // 벽 제거
+        if (this.walls[i].isOutside) {
+          this.walls.splice(i, 1);
+          continue;
+        }
+
+        // 벽 생성
+        if (this.walls[i].canGenerateNext) {
+          this.walls[i].generatedNext = true;
+          const newWall = new Wall({
+            type: Math.random() > 0.3 ? "SMALL" : "BIG",
+          });
+          this.walls.push(newWall);
+
+          // 코인 생성 관련
+          if (Math.random() < 1) {
+            const x = newWall.x + newWall.width / 2;
+            const y = newWall.y2 - newWall.gapY / 2;
+            this.coins.push(new Coin(x, y, newWall.vx));
+          }
+        }
+
+        // 벽과 플레이어 충돌관련 주석
+        if (this.walls[i].isColliding(this.player.boundingBox)) {
+          this.gameHandler.status = "FINISHED";
+          break;
+        }
+      }
+
+      // 플레이어 관련
+      this.player.update();
+      this.player.draw();
+
+      if (
+        this.player.y >= App.height ||
+        this.player.y + this.player.height <= 0
+      ) {
+        this.gameHandler.status = "FINISHED";
+      }
+
+      // 코인 관련
+      for (let i = this.coins.length - 1; i >= 0; i--) {
+        this.coins[i].update();
+        this.coins[i].draw();
+
+        if (this.coins[i].x + this.coins[i].width < 0) {
+          this.coins.splice(i, 1);
+          continue;
+        }
+
+        // 플레이어와 코인의 충돌
+        if (this.coins[i].boundingBox.isColliding(this.player.boundingBox)) {
+          this.score.coinCount += 1;
+          this.coins.splice(i, 1);
+        }
+      }
+
+      // 스코어 관련
+      this.score.update();
+      this.score.draw();
 
       then = now - (delta % App.interval);
     };
---
+++ lectures/part2/5. 미니게임/js/utils.js
@@ -0,0 +1,4 @@
+//@ts-nocheck
+export const randomNumBetween = (min, max) => {
+  return Math.random() * (max - min) + min;
+};
